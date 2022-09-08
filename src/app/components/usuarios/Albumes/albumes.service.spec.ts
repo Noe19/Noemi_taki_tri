9d3c75b3fd4b165a7552fd1d6dd2c1ba2fd744e1
Noe19 <65981417+Noe19@users.noreply.github.com>
2022-09-08 16:49:25 -0500

    Update albumes.service.spec.ts

--- src/app/components/usuarios/Albumes/albumes.service.spec.ts
+++ src/app/components/usuarios/Albumes/albumes.service.spec.ts
@@ -1,37 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlbumesService } from './albumes.service';
-/*
-describe('AlbumesService', () => {
-  let service: AlbumesService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AlbumesService);
-  });
 
-  it('albumes', () => {
-    expect(service).toBeTruthy();
-  });
-  
-});
-*/
-//import firebaseFunctionsTest from "firebase-functions-test";
 describe('Pruebas creacion album', () =>{
   let fixture: ComponentFixture<AlbumesService>;
   let component: AlbumesService;
 
 
 it('Prueba de Album', () =>{
- /*
+ 
 fixture = TestBed.createComponent(AlbumesService);
 component = fixture.componentInstance;
 const datos_albumes = {name: "Muñequita",author:"Pamela Paez",year:"2015",
 imageURL:"https://firebasestorage.googleapis.com/v0/b/takitesis.appspot.com/o/imagenAlbumes%2F8dOfzRCcaJeCBk0SjI6mRxSmTZA2%2FGuerra?alt=media&token=502c8da0-91a2-4ad9-8daa-6faed7123d4e"};
 const verification = component.crearAlbumes(datos_albumes);
 expect(verification).toBeTruthy();
-*/
-let veri = true
-expect(veri).toBeTruthy()
+
+
 })
 })
