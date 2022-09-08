5ea386c0f39f89616a8f308eb6925f66e8b04b83
Noe19 <65981417+Noe19@users.noreply.github.com>
2022-09-08 16:51:29 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyD_MdB7V84IYoY9yggWQ3yCWmEJMylIpO8",
-  authDomain: "borrador-a0724.firebaseapp.com",
-  projectId: "borrador-a0724",
-  storageBucket: "borrador-a0724.appspot.com",
-  messagingSenderId: "177835894815",
-  appId: "1:177835894815:web:c20484a66ef6ec27068d2a"
+    apiKey: "*********************************",
+  authDomain: "***************************",
+  projectId: "****************************",
+  storageBucket: "***********************",
+  messagingSenderId: "*************************",
+  appId: "************************************"
   },
   production: true
 };
