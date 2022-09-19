b8241e2dead278779c1495515f65a93cd054d3e3
Noe19 <65981417+Noe19@users.noreply.github.com>
2022-09-18 19:52:46 -0500

    Update albumes.service.spec.ts

--- src/app/components/usuarios/Albumes/albumes.service.spec.ts
+++ src/app/components/usuarios/Albumes/albumes.service.spec.ts
@@ -15,7 +15,7 @@ describe('AlbumesService', () => {
   });
   
 });
-*/
+
 //import firebaseFunctionsTest from "firebase-functions-test";
 describe('Pruebas creacion album', () =>{
   let fixture: ComponentFixture<AlbumesService>;
@@ -23,15 +23,14 @@ describe('Pruebas creacion album', () =>{
 
 
 it('Prueba de Album', () =>{
- /*
+ 
 fixture = TestBed.createComponent(AlbumesService);
 component = fixture.componentInstance;
 const datos_albumes = {name: "Muñequita",author:"Pamela Paez",year:"2015",
 imageURL:"https://firebasestorage.googleapis.com/v0/b/takitesis.appspot.com/o/imagenAlbumes%2F8dOfzRCcaJeCBk0SjI6mRxSmTZA2%2FGuerra?alt=media&token=502c8da0-91a2-4ad9-8daa-6faed7123d4e"};
 const verification = component.crearAlbumes(datos_albumes);
 expect(verification).toBeTruthy();
-*/
-let veri = true
-expect(veri).toBeTruthy()
+
+
 })
 })
